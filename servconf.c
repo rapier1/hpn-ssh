b60b4bb7dd208efa4f22a95738303e8dd5184c39
Chris Rapier <rapier@psc.edu>
2025-09-22 17:38:47 -0400

    Typo in servconf.c

--- servconf.c
+++ servconf.c
@@ -497,7 +497,7 @@ fill_default_server_options(ServerOptions *options)
 	if (options->ip_qos_interactive == -1)
 		options->ip_qos_interactive = IPTOS_DSCP_AF21;
 	if (options->ip_qos_bulk == -1)
-		options->ip_qos_bulk = IPTOS_DCSP_CS1;
+		options->ip_qos_bulk = IPTOS_DSCP_CS1;
 	if (options->version_addendum == NULL)
 		options->version_addendum = xstrdup("");
 	if (options->fwd_opts.streamlocal_bind_mask == (mode_t)-1)
