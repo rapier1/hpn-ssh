49a4e78b19ee46717b031893071580f27e4eec6e
Chris Rapier <rapier@psc.edu>
2026-04-24 14:25:00 -0400

    Update regression test for scp-resume and remove stray debug check from sftp-resume.sh

--- regress/sftp-resume.sh
+++ regress/sftp-resume.sh
@@ -30,8 +30,6 @@ for cmd in put get; do
 
 	# Perform copy and check.
 
-	echo "CMD = ${cmd}"
-
 	echo "${cmd} -a ${COPY}.1 ${COPY}.2" | \
 	    ${SFTP} -D ${SFTPSERVER} -vvv >${CLIENT_LOG} 2>&1 \
 	    || fail "${cmd} failed"
