8a0bdcf3a79dfc0056a2f7a66b888d474bb4e31f
Chris Rapier <rapier@psc.edu>
2026-09-11 16:14:18 -0400

    regress/integrity.sh. 3000 was still triggering issues on Ubuntu. Upped to 3500.

--- regress/integrity.sh
+++ regress/integrity.sh
@@ -4,9 +4,9 @@
 tid="integrity"
 cp $OBJ/sshd_proxy $OBJ/sshd_proxy_bak
 
-# start at byte 3000 (i.e. after kex) and corrupt at different offsets
+# start at byte 4500 (i.e. after kex) and corrupt at different offsets
 tries=10
-startoffset=3000
+startoffset=3500
 macs=`${SSH} -Q mac`
 
 # The following are not MACs, but ciphers with integrated integrity. They are
